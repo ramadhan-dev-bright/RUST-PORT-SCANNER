33da027d6f616007eb320c7acdb7f6af925105fa
MUHAMMAD_RAMADHAN <ramadhanmadhan560@gmail.com>
2026-01-27 17:54:58 +0700

    Refactor main function for improved readability

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,8 @@ async fn main() {
     let args: Vec<String> = env::args().collect();
     let target_str = args.get(1).map(|s| s.as_str()).unwrap_or("127.0.0.1");
     let target: IpAddr = target_str.parse().expect("IP Address tidak valid!");
-
     let concurrency = 100; 
     let timeout = Duration::from_millis(200); 
-
     println!("{}", "========================================".cyan());
     println!("{}", "   [ NEURON PORT SCANNER RUST V20.2 ]".bold().white());
     println!("   TARGET: {} | MODE: PARALLEL", target.to_string().yellow());
